fc27748006130f615de8fff91a596093c249e20b
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-08-11 21:20:51 +0200

    Add files via upload

---
+++ NotCppProject/NotCppProject.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    std::wcout << "Tell me anything you want!" << std::endl;
+    std::string a;
+    std::getline(std::cin, a);
+    std::cout << "You want: " << a << std::endl;
+    system("pause");
+}
